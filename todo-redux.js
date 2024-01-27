5dc3e9294029b00eda58e46f0a14393c7eca6e48
VRAJ9R5 <rahul.raj2@pearson.com>
2024-01-28 00:18:11 +0530

    Adding action creatore initial state and reducer function at first level

--- todo-redux.js
+++ todo-redux.js
@@ -6,3 +6,27 @@ const ADD_TODO="Add TODO"
 const TOGGLE_TODO="Toggle TODO"
 
 
+// Action Creater
+
+const addToDo = (text)=>({text,type:ADD_TODO})
+const toggleToDo = (index)=>({index,type:TOGGLE_TODO})
+
+/**Initial state */
+
+const initialState={
+  todo:[]
+}
+
+// Reducer
+
+/**Reducer is a pure function */
+
+function todoReducer(state=initialState,action){
+  switch (action.type) {
+    case ADD_TODO :
+    case TOGGLE_TODO:
+  
+    default:
+      return state
+  }
+}
